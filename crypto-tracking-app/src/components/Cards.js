b5bf58145999c35029862225f3abe4c5faff567b
Gokul <gokulgokul8351@gmail.com>
2023-07-31 09:24:15 +0530

    first commit

--- crypto-tracking-app/src/components/Cards.js
+++ crypto-tracking-app/src/components/Cards.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function imageCards(props) {
+export default function imageCards(val) {
   return (
     <div className="p-2">
       <div
@@ -9,18 +9,18 @@ export default function imageCards(props) {
       >
         <div className="justify-content-center">
           <img
-            src={props.image}
-            alt={props.name}
+            src={val.image}
+            alt={val.name}
             width="80"
             height="80"
             className="image rounded mx-auto d-block"
           />
         </div>
         <div className="card-body mx-auto d-block">
-          <h5 className="card-title">{props.name}</h5>
-          <p>Price : {`$${props.current_price}`}</p>
-          <p>Rank : {props.market_cap_rank}</p>
-          <p>Market Cap: ${props.market_cap} </p>
+          <h5 className="card-title">{val.name}</h5>
+          <p>Price : {`$${val.current_price}`}</p>
+          <p>Rank : {val.market_cap_rank}</p>
+          <p>Market Cap: ${val.market_cap} </p>
           <button></button>
         </div>
       </div>
